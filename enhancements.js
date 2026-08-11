b585891fbf0c36c0aefc82058fa0dea0de3c87c2
naztutnet <snegproduction@gmail.com>
2026-08-11 14:12:23 +0300

    Improve homepage readability and mobile navigation

--- enhancements.js
+++ enhancements.js
@@ -23,7 +23,7 @@
   }, true);
 
   function ensureStyles() {
-    for (const href of ['product.css?v=2026081106', 'product-home.css?v=2026081106', 'white-theme.css?v=2026081107', 'approved-home.css?v=2026081109']) {
+    for (const href of ['product.css?v=2026081106', 'product-home.css?v=2026081106', 'white-theme.css?v=2026081107', 'approved-home.css?v=2026081116']) {
       if ([...document.styleSheets].some(s => s.href?.includes(href.split('?')[0]))) continue;
       const link = document.createElement('link');
       link.rel = 'stylesheet';
